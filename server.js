60dcb735631edb918794965e8c1deffe14d00548
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-08 17:53:30 -0500

    joined tables

--- server.js
+++ server.js
@@ -50,7 +50,11 @@ app.post("/api/candidate", ({ body }, res) => {
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql =  `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -67,8 +71,12 @@ app.get("/api/candidates", (req, res) => {
 
 // Get single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+  const sql =  `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
